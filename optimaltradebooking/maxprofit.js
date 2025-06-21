d2f72ca69ea9f9e2a2b2bfe34cbd9fa32574818b
mehedi <mehedibu2013@gmail.com>
2025-06-21 10:36:08 +0600

    Optimal trade booking

--- optimaltradebooking/maxprofit.js
+++ optimaltradebooking/maxprofit.js
@@ -1,11 +1,26 @@
 
 function maxProfit(prices) {
-  let minPrice = prices[0];
-  let maxProfit = 0;
+    if (prices.length === 0) {
+        return "No prices provided";
+    }
+    if (!prices.every((price) => typeof price === "number")) {
+        return "Prices must be numbers";
+    }
+   
+    if (prices.length === 1) {
+        return "No profit can be made";
+    }
+    if (!Array.isArray(prices)) {
+        return "Prices must be an array";
+    }
 
-  for (let i = 1; i < prices.length; i++) {
-    minPrice = Math.min(minPrice, prices[i]);
-    maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+    
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        minPrice = Math.min(minPrice, prices[i]);
+        maxProfit = Math.max(maxProfit, prices[i] - minPrice);
   }
 
   return maxProfit;
@@ -17,3 +32,11 @@ const prices2 = [7, 1, 5, 3, 6, 4];
 console.log(maxProfit(prices2)); // Expected: 5
 const prices3 = [7, 6, 4, 3, 1];
 console.log(maxProfit(prices3));
+const prices4 = [];
+console.log(maxProfit(prices4));
+const prices5 = [1];
+console.log(maxProfit(prices5));
+const prices6 = [1, "h"];
+console.log(maxProfit(prices6));
+const prices7 = ["hello"];
+console.log(maxProfit(prices7));
